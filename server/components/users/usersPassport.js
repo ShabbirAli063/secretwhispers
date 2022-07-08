4b63dfb4b9b7944ad9feeab1ee4bef909b9f0b36
ShabbirAli063 <99862924+ShabbirAli063@users.noreply.github.com>
2022-07-08 16:36:37 +0530

    Update usersPassport.js

--- server/components/users/usersPassport.js
+++ server/components/users/usersPassport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const secretOrKey = 'gdfggADAEDW4WR4324feds';
+const secretOrKey = 'gafdfsddfggADAEDW4WR4324feds';
 
 
 const opts = {
